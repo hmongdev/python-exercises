13f3ff7eddfb9c6768a91a6586917cec0ec8e26f
yang2674 <steve@softwareforgood.com>
2022-03-26 17:56:37 -0500

    math.py

---
+++ math.py
@@ -0,0 +1,32 @@
+#/////////////////////////math///////////////////////////
+#concepts: operators, augmented assignment operator, comparison, logical
+
+print(10 + 3) #//13
+print(10 - 3) #//7
+print(10 * 3) #//30
+print(10 ** 3) #//1_000, exponent
+print(10 / 3) #//3.3333333333333333, decimal
+print(10 // 3) #//3; this one just returns the integer, not the decimal part of it
+print(10 % 3) #//1, modulus operator returns only the remainder
+
+#augmented assignment operator
+# x = 10
+# (x = x + 3) is the same as (x += 3) #//13
+
+# /////////////////////////operator precedence///////////////////////////
+# this is the same idea as PEMDAS, order of operations
+# to change operator precedence, just use parentheses (10 + 3) * 2
+
+# /////////////////////////comparison operators///////////////////////////
+# comparison operators
+# >   greater than
+# >=  greater than or equal to
+# <   less than
+# <=  less than or equal to
+# ==  equal to
+# !=  not equal to
+
+# /////////////////////////logical operators///////////////////////////
+# and (true only if both conditiosn are true)
+# or  (true if at least one condition is true)
+# not (inverse the boolean)
