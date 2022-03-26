9d9c754b7c2ec69d08095f06ad75dbc615d4eaf5
yang2674 <steve@softwareforgood.com>
2022-03-26 18:01:16 -0500

    variables_inputs.py

---
+++ variables_inputs.py
@@ -0,0 +1,16 @@
+#/////////////////////////variables///////////////////////////
+age = 29
+ethnicity = "Hmong-American"
+
+# multiple word variables are separated by _ (underscore) and no capital letters
+first_name = 'Avery'
+last_name = 'Yang'
+
+# boolean values are case-sensitive
+is_online = True 
+# is_online = true //true will cause an error
+
+
+#/////////////////////////inputs///////////////////////////
+name = input('What is your name? ')
+print('Hello '+name) #print is a general method that gives us what's inside it. 'Hello' +name is string concatenation
